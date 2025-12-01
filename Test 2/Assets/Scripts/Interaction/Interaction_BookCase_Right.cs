71f0dc2ff0874642196aa38949b7662917492c8b
sinsael <101236411+sinsael@users.noreply.github.com>
2025-12-01 20:03:19 +0900

    12-01

--- Test 2/Assets/Scripts/Interaction/Interaction_BookCase_Right.cs
+++ Test 2/Assets/Scripts/Interaction/Interaction_BookCase_Right.cs
@@ -1,16 +1,52 @@
+using Cinemachine;
 using UnityEngine;
+using System.Collections;
 
-public class Interaction_BookCase_Right : MonoBehaviour
+public class Interaction_BookCase_Right : Interaction_Obj
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CinemachineVirtualCamera targetCam;
+    private CinemachineConfiner2D confiner;
+    public GameObject FollowTarget;
+    public float targetZoomSize = 3f;
+    public float zoomDuration = 3f;
+    public override void Start()
     {
-        
+        base.Start();
+
+        GameObject obj = GameObject.Find("Second");
+
+        targetCam = obj.GetComponent<CinemachineVirtualCamera>();
+        confiner = obj.GetComponent<CinemachineConfiner2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInteract(PlayerInputHandler PlayerInput)
     {
-        
+        base.OnInteract(PlayerInput);
+
+        if (targetCam != null)
+        {
+            targetCam.Follow = FollowTarget.transform;
+
+            // 코루틴 시작 (부드러운 줌)
+            StartCoroutine(SmoothZoom(targetZoomSize, zoomDuration));
+        }
+    }
+
+    IEnumerator SmoothZoom(float targetSize, float duration)
+    {
+        float startSize = targetCam.m_Lens.OrthographicSize;
+        float time = 0;
+
+        while (time < duration)
+        {
+            // Lerp를 이용해 서서히 값 변경
+            targetCam.m_Lens.OrthographicSize = Mathf.Lerp(startSize, targetSize, time / duration);
+            time += Time.deltaTime;
+            confiner.InvalidateCache();
+            yield return null;
+        }
+
+        // 마지막에 정확한 값으로 고정
+        targetCam.m_Lens.OrthographicSize = targetSize;
     }
 }
