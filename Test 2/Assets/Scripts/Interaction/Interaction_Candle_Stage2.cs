d20af9f477dc199d985d17de1d5c8314d0a7f11b
sinsael <101236411+sinsael@users.noreply.github.com>
2025-12-03 22:33:55 +0900

    12-03

--- Test 2/Assets/Scripts/Interaction/Interaction_Candle_Stage2.cs
+++ Test 2/Assets/Scripts/Interaction/Interaction_Candle_Stage2.cs
@@ -1,16 +1,12 @@
-using System.Security.Cryptography;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interaction_Candle_Stage2 : Interaction_Obj
 {
     SpriteRenderer spriteRenderer;
-    [SerializeField] Sprite[] candleSprite;
+    [SerializeField] Sprite[] candleSprite = null;
     [SerializeField] Interaction_BookCase_Right IBR;
-
-    [Header("Candle Settings")]
-    public int candleIndex;
-    private bool isCandleOn = false;
+    
+    public Color myCandleColor = Color.white;
 
     public override void Start()
     {
@@ -20,26 +16,45 @@ public class Interaction_Candle_Stage2 : Interaction_Obj
 
         if (IBR == null)
             IBR = FindFirstObjectByType<Interaction_BookCase_Right>();
+
+        ResetCandle();
     }
 
     public override void OnInteract(PlayerInputHandler PlayerInput)
     {
-
-        isCandleOn = true;
-        UpdateVisual();
-    }
-
-    public void ForceTurnOff()
-    {
-        isCandleOn = false;
-        UpdateVisual();
-    }
-
-    void UpdateVisual()
-    {
-        if (candleSprite != null && candleSprite.Length >= 2)
+        if (IBR != null && IBR.isActive && IBR.isSequence)
         {
-            spriteRenderer.sprite = isCandleOn ? candleSprite[1] : candleSprite[0];
+            // 2. 책장에게 "이 색깔 맞아?" 라고 물어봄 (채점 요청)
+            bool isCorrect = IBR.CheckPuzzleAnswer(myCandleColor);
+
+            if (isCorrect)
+            {
+                // 정답이면 불을 켬
+                base.OnInteract(PlayerInput); // 효과음 등
+                LightOn();
+            }
+            else
+            {
+                // 틀리면 책장이 알아서 닫히므로 촛불은 할 게 없음 (로그만 출력)
+                Debug.Log("틀렸습니다! 초기화됩니다.");
+            }
+        }
+    }
+    // 모든 양초 초기화
+    void LightOn()
+    {
+        if (candleSprite != null && candleSprite.Length > 1)
+        {
+            spriteRenderer.sprite = candleSprite[1]; // 켜진 이미지
+        }
+    }
+
+    // 불 끄기 (초기화)
+    public void ResetCandle()
+    {
+        if (candleSprite != null && candleSprite.Length > 0)
+        {
+            spriteRenderer.sprite = candleSprite[0]; // 꺼진 이미지
         }
     }
 }
