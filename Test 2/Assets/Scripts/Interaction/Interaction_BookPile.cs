bdef652b198073fd7520b5693a376098b35291d4
call4cc <astopaz206@gmail.com>
2025-12-08 10:25:12 +0900

    12-08
    12-08
    - 1 스테이지 완성

--- Test 2/Assets/Scripts/Interaction/Interaction_BookPile.cs
+++ Test 2/Assets/Scripts/Interaction/Interaction_BookPile.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Interaction_BookPile : Interaction_Obj
 {
+    [Header("상호작용 시 획득하는 아이템")]
+    [SerializeField] private ItemData pickItem;
+
     private bool isUsed = false;
 
     public override void OnInteract(PlayerInputHandler interactor)
@@ -11,6 +14,19 @@ public class Interaction_BookPile : Interaction_Obj
 
         if (isUsed)
             return;
+
+        if (pickItem == null)
+        {
+            Debug.LogWarning("획득할 아이템 선택하기");
+            return;
+        }
+
+        bool added = Inventory.Instance.AddList(pickItem);
+        if (!added)
+        {
+            Debug.Log("인벤토리가 가득 차서 아이템을 획득할 수 없습니다.");
+            return;
+        }
 
         isUsed = true;
 
