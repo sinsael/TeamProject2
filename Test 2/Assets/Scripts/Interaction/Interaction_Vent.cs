c1ae006175ad59ca83ccaacf63c598a34b72e2c8
call4cc <astopaz206@gmail.com>
2025-12-17 04:18:01 +0900

    17-12

--- Test 2/Assets/Scripts/Interaction/Interaction_Vent.cs
+++ Test 2/Assets/Scripts/Interaction/Interaction_Vent.cs
@@ -1,38 +1,61 @@
 using UnityEngine;
 
-public class Interaction_Vent : Interaction_Obj // 다음 스테이지로 넘어가기 위한 환풍구
+// 다음 스테이지로 넘어가기 위한 환풍구
+public class Interaction_Vent : Interaction_Obj
 {
     [SerializeField] private Collider2D ventCollider;
 
     private AltarUIManager altar;
+    private bool CompletEvent;
 
     private void Awake()
     {
+        // 콜라이더가 자식에 있을 수도 있어서 자식까지 포함해서 찾기
         if (ventCollider == null)
             ventCollider = GetComponent<Collider2D>();
+
+        if (ventCollider != null)
+            ventCollider.enabled = false;
     }
 
-    private void Start()
+    public override void Start()
     {
-        altar = AltarUIManager.Instance;
-        //if (altar == null)
-        //    altar = FindObjectOfType<AltarUIManager>(true);
+        base.Start();
 
-        if (altar == null)
-        {
-            if (ventCollider != null)
-                ventCollider.enabled = false;
-            return;
-        }
-
-        altar.OnAltarCompleteChanged += HandleAltarCompleteChanged;
-        HandleAltarCompleteChanged(altar.IsAltarComplete());
+        TrySubscribeAltar();
     }
+
 
     private void OnDisable()
     {
+        UnsubscribeAltar();
+    }
+
+    private void TrySubscribeAltar()
+    {
+        altar = AltarUIManager.Instance;
+        if (altar == null)
+            return;
+
+        if (CompletEvent)
+            return;
+
+        altar.OnAltarCompleteChanged += HandleAltarCompleteChanged;
+        CompletEvent = true;
+
+        HandleAltarCompleteChanged(altar.IsAltarComplete());
+    }
+
+    private void UnsubscribeAltar()
+    {
+        if (!CompletEvent)
+            return;
+
         if (altar != null)
             altar.OnAltarCompleteChanged -= HandleAltarCompleteChanged;
+
+        CompletEvent = false;
+        altar = null;
     }
 
     private void HandleAltarCompleteChanged(bool complete)
